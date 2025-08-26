76884f907e4e6a7d65419b0110b17f597f58a6b6
Barkha <barkhaps3009@gmail.com>
2025-08-26 10:55:23 +0530

    changes so far

--- src/components/ErrorBoundary.tsx
+++ src/components/ErrorBoundary.tsx
@@ -4,10 +4,10 @@
 import React from "react";
 
 export default class ErrorBoundary extends React.Component<
-  { fallback?: React.ReactNode },
+  { fallback?: React.ReactNode; children?: React.ReactNode },
   { error: Error | null }
 > {
-  constructor(props: { fallback?: React.ReactNode }) {
+  constructor(props: { fallback?: React.ReactNode; children?: React.ReactNode }) {
     super(props);
     this.state = { error: null };
   }
